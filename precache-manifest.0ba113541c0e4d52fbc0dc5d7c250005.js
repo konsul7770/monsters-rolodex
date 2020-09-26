a0debbffacb90d5b98b9b5a6808b11826d0ce6e6
konsul777 <konsul7770@gmail.com>
2020-09-26 18:32:38 +0300

    Updates

---
+++ precache-manifest.0ba113541c0e4d52fbc0dc5d7c250005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7fd4e356e468888e654e3f9ad7f450",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d9b3db8f127083ba8fa9",
+    "url": "/monsters-rolodex/static/css/main.72df07bf.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b3db8f127083ba8fa9",
+    "url": "/monsters-rolodex/static/js/main.92ab1b91.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
